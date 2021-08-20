7d3166fa8502cf2fe90d563a0b4e2883c217a45f
jairam madanala <madanala.jairam@onpassive.com>
2021-08-20 16:21:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import AllUsers from './Component/AllUsers';
+import AddUser from './Component/AddUser';
+import EditUser from './Component/EditUser';
+import NavBar from './Component/NavBar';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Switch>
+        <Route exact path="/all" component={AllUsers} />
+        <Route exact path="/add" component={AddUser} />
+        <Route exact path="/edit/:id" component={EditUser} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
